c910f09b96f5a02b2af5370e185f5344d39633cb
Wisler Michel <wislermichel@Wislers-iMac-4.home>
2019-10-01 09:08:38 -0400

    added buttons images

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+// JavaScript function that wraps everything
+$(document).ready(function () {
+    // At the start of the game a random number will be initiated
+
+    // There are 4 crystals, clicking on a crystal will a specific amount of points to the total score.
+
+    // The value of each crystal is hidden until it's clicked on
+
+    // You win the game by matching the total score  to the random number.
+
+    // You loose the game if the total score is bigger than the random number.
+
+    // Each time the game starts, a new random number for will be generated 
+
+    // New random number for the crystals.
+});
